6de5a7fa86753378a3f3c75888ac67773a299934
LegalFlux <suporte@legalflux.pt>
2025-03-24 22:07:32 +0000

    Update card.tsx

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
 
-const Card = ({ children, ...props }) => {
-  return <div {...props}>{children}</div>;
+interface CardProps {
+  children: React.ReactNode;
+  className?: string;
+}
+
+const Card = ({ children, className = '' }: CardProps) => {
+  return (
+    <div className={`p-6 border border-gray-200 rounded-xl bg-white ${className}`}>
+      {children}
+    </div>
+  );
 };
 
 export default Card;
