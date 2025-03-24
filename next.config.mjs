797b9ce4ea5b8c425490d9a9fe0921b73cbe986b
LegalFlux <suporte@legalflux.pt>
2025-03-24 22:39:35 +0000

    Create next.config.mjs

---
+++ next.config.mjs
@@ -0,0 +1,13 @@
+// next.config.mjs
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  swcMinify: false,
+};
+
+export default nextConfig;
