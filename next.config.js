bff9f0db15cd45b56a2e48a1a5c80cd0fbb91df5
LegalFlux <suporte@legalflux.pt>
2025-03-24 21:54:07 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,23 +2,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
-    // Isso ignora erros de TypeScript durante o build
     ignoreBuildErrors: true,
   },
-  // Desativar verificação de módulos não encontrados
-  webpack: (config) => {
-    // Modificar a configuração do webpack para ignorar erros de módulos não encontrados
-    config.ignoreWarnings = [/Failed to parse source map/];
-    
-    // Configurar o modo de tratamento de erros para 'warn' em vez de 'error'
-    config.optimization = {
-      ...config.optimization,
-      emitOnErrors: true,
-    };
-    
-    return config;
+  eslint: {
+    ignoreDuringBuilds: true,
   },
-  // Desativar verificação estrita de exportações
+  swcMinify: false,
   experimental: {
     esmExternals: 'loose',
   },
