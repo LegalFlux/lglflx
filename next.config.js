89803e93ddcc69ed16c8ac155cd8ef3c17973fcd
LegalFlux <suporte@legalflux.pt>
2025-03-24 21:28:00 +0000

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  typescript: {
+    // Isso ignora erros de TypeScript durante o build
+    ignoreBuildErrors: true,
+  },
+};
+
+module.exports = nextConfig;
