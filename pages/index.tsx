eccfcdf931ebd60894cd534badd20d0a17e1b269
legalflux <suporte@legalflux.pt>
2025-03-28 15:08:08 +0000

    refactor(pages/index): dynamically import Home component with SSR disabled
    Dynamically import the Home component in `pages/index.tsx` to disable server-side rendering (SSR) for improved client-side performance. This change ensures that the Home component is only rendered on the client, reducing unnecessary server load and improving initial page load times.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,10 @@
-import Home from '../src/pages/Home';
+import dynamic from 'next/dynamic';
 
-export default Home;
+// Importa o componente Home apenas no lado do cliente
+const HomeWithNoSSR = dynamic(() => import('../src/pages/Home'), {
+  ssr: false, // Desativa a renderização do lado do servidor
+});
+
+export default function Index() {
+  return <HomeWithNoSSR />;
+}
